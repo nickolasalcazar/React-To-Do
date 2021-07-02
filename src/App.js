594bc4eb43308abf4c642e85e4d1b178e11e1f05
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-07-02 10:53:30 -0700

    Added state to component App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 import PropTypes from 'prop-types'
 
-function App() {
+const App = () => {
+  // App-level state
+  const [tasks, setTasks] = useState([
+      {
+          id: 1,
+          text: 'Go to grocery store'
+      },
+      {
+          id: 2,
+          text: 'Have lunch with friends'
+      },
+      {
+          id: 3,
+          text: 'Go to meeting'
+      }
+    ])
+
   return (
     <div className='container'>
       <Header title={ 'Task Tracker' }/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
